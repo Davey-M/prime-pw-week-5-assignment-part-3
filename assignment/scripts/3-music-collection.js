a0dd92fe4df7df4dd44d2c98650d68424129e770
Davey-M <davey.meuer@gmail.com>
2022-01-12 19:15:02 -0600

    added colection and addToCollection Function.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,12 @@
 console.log('***** Music Collection *****')
+
+const collection = [];      // Music Collection
+
+function addToCollection(title, artist, yearPublished) {
+    let object = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    };
+    return object;
+}
