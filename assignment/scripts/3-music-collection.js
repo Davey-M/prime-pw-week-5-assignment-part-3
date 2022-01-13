8590c6c78338a04aae54b27031508a3433932047
Davey-M <davey.meuer@gmail.com>
2022-01-12 19:26:26 -0600

    added albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,6 @@
 console.log('***** Music Collection *****')
 
-const collection = [];      // Music Collection
+let collection = [];      // Music Collection
 
 function addToCollection(title, artist, yearPublished) {
     let object = {
@@ -8,5 +8,18 @@ function addToCollection(title, artist, yearPublished) {
         artist: artist,
         yearPublished: yearPublished,
     };
+    collection.push(object);
     return object;
 }
+
+console.log(`Added ablum:`, addToCollection('Stoney', 'Post Malone', 2016));
+
+console.log(`Added ablum:`, addToCollection('Circles', 'Mac Miller', 2020));
+
+console.log(`Added ablum:`, addToCollection('Mr', 'Leslie Odom Jr.', 2019));
+
+console.log(`Added ablum:`, addToCollection('Bearbongs & Bentleys', 'Post Malone', 2018));
+
+console.log(`Added ablum:`, addToCollection('PTX, Vol, IV: Classics', 'Pentatonix', 2017));
+
+console.log(`Added ablum:`, addToCollection('The Search', 'NF', 2019));
